651de594cd2b302502652215974724ee6962a032
Joona Tuomikoski <joonat96@gmail.com>
2024-02-16 20:29:23 +0200

    - Task 1.3 done

--- Osa_1/task1/src/App.jsx
+++ Osa_1/task1/src/App.jsx
@@ -14,9 +14,9 @@ const Content = (props) => {
     const contentArray = props.contents
     return (
         <div>
-            <Part part={contentArray[0].part} exercises={contentArray[0].exercises} />
-            <Part part={contentArray[1].part} exercises={contentArray[1].exercises} />
-            <Part part={contentArray[2].part} exercises={contentArray[2].exercises} />
+            <Part part={contentArray[0].name} exercises={contentArray[0].exercises} />
+            <Part part={contentArray[1].name} exercises={contentArray[1].exercises} />
+            <Part part={contentArray[2].name} exercises={contentArray[2].exercises} />
         </div>
     )
 }
@@ -40,21 +40,23 @@ const Part = (props) => {
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const part1 = {
+        name: 'Fundamentals of React',
+        exercises: 10
+    }
+    const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7
+    }
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14
+    }
 
-    const contentArray = [
-        { part: part1, exercises: exercises1 },
-        { part: part2, exercises: exercises2 },
-        { part: part3, exercises: exercises3 }
-    ];
+    const contentArray = [part1, part2, part3];
 
     const totalString = 'Number of exercises'
-    const totalSum = exercises1 + exercises2 + exercises3;
+    const totalSum = part1.exercises + part2.exercises + part3.exercises;
 
     return (
         <div>
