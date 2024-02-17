e2c1e6e925328aacc8cd3cfa7b9abd439e58d2c1
Joona Tuomikoski <joonat96@gmail.com>
2024-02-17 15:27:56 +0200

    - Task 1.14 done, anekdootit step 3

--- Osa_1/anekdootit/src/App.jsx
+++ Osa_1/anekdootit/src/App.jsx
@@ -1,5 +1,14 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import { useState } from 'react'
+
+const Header = ({ headerText }) => {
+    return (
+        <div>
+            <h1>{headerText}</h1>
+        </div>
+    )
+}
 
 const App = () => {
     const anecdotes = [
@@ -16,6 +25,9 @@ const App = () => {
     const [votesArray, setVotesArray] = useState(new Array(8).fill(0));
     const [selected, setSelected] = useState(0)
 
+    const maxIndex = votesArray.indexOf(Math.max(...votesArray));
+    const maxValue = votesArray[maxIndex];
+
     const increaseVotes = () => {
         const newVotesArray = [...votesArray]
         newVotesArray[selected] += 1
@@ -24,12 +36,16 @@ const App = () => {
 
     return (
         <div>
+            <Header headerText="Anecdote of the day" />
             {anecdotes[selected]}
             <div>has {votesArray[selected]} votes</div>
             <div>
                 <button onClick={() => increaseVotes()}>Vote</button>
                 <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>Next anecdote</button>
             </div>
+            <Header headerText="Anecdote with the most votes" />
+            <div>{anecdotes[maxIndex]}</div>
+            <div>has {maxValue} votes</div>
         </div>
     )
 }
