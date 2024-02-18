0ecb7065236df378f74e0e5b0e37de3f115c1681
Joona Tuomikoski <joonat96@gmail.com>
2024-02-19 00:03:30 +0200

    - Task 2.11 done, puhelinluettelo step6

--- Osa_2/puhelinluettelo/src/App.jsx
+++ Osa_2/puhelinluettelo/src/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ value, onChange }) => {
     return (
@@ -41,15 +42,22 @@ const Person = ({ person }) => {
 }
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [newFilterInput, setNewFilterInput] = useState('')
+
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                console.log('promise fulfilled')
+                setPersons(response.data)
+            })
+    }, [])
+
+    console.log('render', persons.length, 'persons')
 
     const addNumber = (event) => {
         event.preventDefault()
